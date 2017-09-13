b62b4570cf811e6a664837dcc01a702cfa078f5b
Jeroen Lankheet <jeroen.lankheet@mysensara.com>
2017-09-13 17:10:02 +0200

    Initial commit

---
+++ src/main/java/com/lankheet/iot/datatypes/Measurement.java
@@ -0,0 +1,46 @@
+package com.lankheet.iot.datatypes;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * A Measurement is a single information item (from a P1 datagram)
+ */
+public class Measurement {
+	@JsonProperty
+	private LocalDateTime timeStamp;
+
+	@JsonProperty
+	private MeasurementType type;
+
+	@JsonProperty
+	private double value;
+
+	public Measurement() {}
+	@JsonCreator
+	public Measurement(@JsonProperty("timeStamp") LocalDateTime timeStamp, @JsonProperty("type") MeasurementType type,
+			@JsonProperty("value") double value) {
+		this.type = type;
+		this.timeStamp = timeStamp;
+		this.value = value;
+	}
+
+	public MeasurementType getType() {
+		return type;
+	}
+
+	public LocalDateTime getTimeStamp() {
+		return timeStamp;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	@Override
+	public String toString() {
+		return "Measurement [timeStamp=" + timeStamp + ", type=" + type + ", value=" + value + "]";
+	}
+}
