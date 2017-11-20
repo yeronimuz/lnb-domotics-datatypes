72d275e883371e9790c0962162834faa05772cab
Jeroen Lankheet <jeroen.lankheet@mysensara.com>
2017-11-20 16:29:44 +0100

    Move JsonUtil to lib project

---
+++ src/main/java/com/lankheet/utils/JsonUtil.java
@@ -0,0 +1,33 @@
+package com.lankheet.utils;
+
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.lankheet.iot.datatypes.Measurement;
+
+public class JsonUtil {
+    private static final Logger LOG = LogManager.getLogger(JsonUtil.class);
+
+    private static final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule())
+            .configure(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+    public static final String toJson(Object o) {
+        try {
+            return mapper.writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            LOG.error("Json Processing was wrong: " + e, LOG);
+        }
+
+        return null;
+    }
+
+    public static Measurement measurementFromJson(String json)
+            throws JsonParseException, JsonMappingException, IOException {
+        return mapper.readValue(json, Measurement.class);
+    }
+}
---
+++ src/test/java/com/lankheet/iot/datatypes/MeasurementTest.java
@@ -0,0 +1,28 @@
+package com.lankheet.iot.datatypes;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import java.io.IOException;
+import java.util.Date;
+import org.junit.Test;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.lankheet.utils.JsonUtil;
+
+public class MeasurementTest {
+//	private static final Logger LOG = LogManager.getLogger(MeasurementTest.class);
+
+	@Test
+	public void test() throws JsonParseException, JsonMappingException, IOException {
+		Date now = new Date();
+		Measurement meas = new Measurement(0, now, MeasurementType.CONSUMED_GAS.getId(), 2.0);
+		String jsonMeasurement = JsonUtil.toJson(meas);
+//		LOG.info(jsonMeasurement);
+		
+		Measurement measBack = JsonUtil.measurementFromJson(jsonMeasurement);
+		assertThat(measBack.getType(), is(MeasurementType.CONSUMED_GAS.getId()));
+		assertThat(measBack.getTimeStamp().compareTo(now), is(0));
+		assertThat(measBack.getValue(), is(2.0));
+	}
+
+}
